f25cf0b3ca491a4454d31919453f408c3ad56992
Jacky yu <ych605@gmail.com>
2023-05-23 11:29:48 +0800

    update tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders react app', () => {
+test("renders react app", () => {
   const { getByText } = render(<App />);
-  const appElement = getByText(/Generic Message Component Challenge/i);
+  const appElement = getByText(/Generic Message Component/i);
   expect(appElement).toBeInTheDocument();
 });
